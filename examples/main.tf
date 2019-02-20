44c536096a8f8f36a75bb129851c0c4587b97817
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2019-02-20 14:01:43 +0000

    Added random provider to generate a unique family name every time the module is applied to a new cluster

--- examples/main.tf
+++ examples/main.tf
@@ -9,10 +9,15 @@ provider "template" {
   version = "~> 2.0"
 }
 
+provider "random" {
+  version = "~> 2.0"
+}
+
 module "cAdvisor" {
   providers = {
     aws      = "aws"
     template = "template"
+    random   = "random"
   }
 
   source     = "../modules/cAdvisor"
--- modules/cAdvisor/01-task.tf
+++ modules/cAdvisor/01-task.tf
@@ -1,6 +1,15 @@
-# Missing /sys because it fails no matter what I try to do
+# Allows to change task name every time and avoid conflicts
+resource "random_string" "suffix" {
+  length  = 32
+  special = false #
+
+  keepers = {
+    cluster = "${var.cluster_id}"
+  }
+}
+
 resource "aws_ecs_task_definition" "cadvisor" {
-  family                = "cadvisor"
+  family                = "cadvisor-${random_string.suffix.result}"
   container_definitions = "${file("${path.module}/task-definitions/cadvisor.json")}"
 
   volume {
